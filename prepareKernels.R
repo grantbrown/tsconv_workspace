9e1f4fc4214d79693d9824c8d8123fa50371db08
Grant Brown <grant.brown73@gmail.com>
2015-06-08 20:41:00 -0500

    prepare workspace

---
+++ prepareKernels.R
@@ -0,0 +1,12 @@
+files = dir("./kernels/")
+
+readKernel = function(kname)
+{
+    paste(readLines(paste("./kernels/", kname, sep = "")), sep = "\n")
+}
+
+out = lapply(files, readKernel)
+names(out) = gsub(".cl", "", files)
+attach(out)
+save(list=names(out), file = "./tsconv/data/kernels.rda", 
+     compress = "bzip2")
